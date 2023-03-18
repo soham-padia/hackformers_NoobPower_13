e0ef12b83a06f96a75afab27e9e07a8a6287751b
Dev Pipalia <pipaliadev@gmail.com>
2023-03-18 18:52:35 +0530

    Initiated firebase with firestore

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { getFirestore,collection,addDoc, doc, getDocs } from 'firebase/firestore'
+import { app, db } from '../context/firebaseconfig'
+
 
 export const HomePage = () => {
+ const[products,setProducts]=useState([])
+ useEffect(()=>{
+  getProducts()
+ },[])
+
+ useEffect(()=>{
+  console.log(products)
+ },[products])
+ 
+ function getProducts(){
+  
+  const productcollectionRef=collection(db, 'ProductData')
+  getDocs(productcollectionRef)
+  .then(response=>{
+    const prod=response.docs.map(doc=>({
+      data:doc.data(),
+      id:doc.id,
+    }))
+    setProducts(prod)
+  })
+  .catch(error=>console.log(error.message))
+}
+
+
+
+
   return (
-    <div>HomePage</div>
+    <div>
+    HomePage
+    <button onClick={()=>getProducts()}>all biscuits</button>
+    <ul>
+      {products.map(product=><li key={product.id}>{product.data.name} {product.data.review} {product.data.price}</li>)}
+    </ul>
+    </div>
   )
 }
